eea586f71c0113f4c97778c1e20e8cadebf09aa5
Connor Reed <c.alexreed@gmail.com>
2025-03-27 02:12:20 -0400

    once more

--- pages/photo/[id].js
+++ pages/photo/[id].js
@@ -6,13 +6,13 @@ import { Section, SectionTitle } from "../../styles/GlobalComponents";
 
 
 export default function photo({photo}){
-    const router = useRouter();
-    if(!router.isFallback && !photo){
-        return <div>ERROR 404 Page Not Found</div>
-    }
+    // const router = useRouter();
+    // if(!router.isFallback && !photo){
+    //     return <div>ERROR 404 Page Not Found</div>
+    // }
     return (
         <Section style={{textAlign: 'center'}}>
-            <SectionTitle>Straight from Nasa.</SectionTitle>
+            {/* <SectionTitle>Straight from Nasa.</SectionTitle>
             <NasaImageContainer>
                 {router.isFallback ? (
                     <div>...loading</div>
@@ -28,34 +28,34 @@ export default function photo({photo}){
                         <NasaButton style={{width: 'max-content'}}>Go Back</NasaButton>
                     </a>
                 </Link>
-            </NasaImageContainer>
+            </NasaImageContainer> */}
         </Section>
     )
 }
 
-export async function getStaticProps({params}){
-    const nasa_id = params.id;
-    const results = await fetch(`https://images-api.nasa.gov/asset/${nasa_id}`);
-    const previews = await results.json();
-    const photo = await previews.collection.items[0].href;
-    return {
-        props: {photo},
-    };
-}
+// export async function getStaticProps({params}){
+//     const nasa_id = params.id;
+//     const results = await fetch(`https://images-api.nasa.gov/asset/${nasa_id}`);
+//     const previews = await results.json();
+//     const photo = await previews.collection.items[0].href;
+//     return {
+//         props: {photo},
+//     };
+// }
 
-export async function getStaticPaths(){
-    const results = await fetch(
-        "https://images-api.nasa.gov/search?media_type=image"
-        );
-    const preview = await results.json();
-    const items = await preview.collection.items;
-    return{
-        paths: 
-        items?.map((nasa) => ({
-            params: {
-                id: nasa.data[0].nasa_id,
-            }
-        })) || [],
-        fallback: true
-    };
-}
+// export async function getStaticPaths(){
+//     const results = await fetch(
+//         "https://images-api.nasa.gov/search?media_type=image"
+//         );
+//     const preview = await results.json();
+//     const items = await preview.collection.items;
+//     return{
+//         paths: 
+//         items?.map((nasa) => ({
+//             params: {
+//                 id: nasa.data[0].nasa_id,
+//             }
+//         })) || [],
+//         fallback: true
+//     };
+// }
